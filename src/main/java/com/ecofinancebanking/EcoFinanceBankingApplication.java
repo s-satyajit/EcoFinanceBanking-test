1def0b9deb3e2bebb2d42ee26056d8125596df94
Satyajit Samal <samalsatyajit668@gmail.com>
2024-03-20 22:25:44 +0530

    Modified some files

---
+++ src/main/java/com/ecofinancebanking/EcoFinanceBankingApplication.java
@@ -0,0 +1,9 @@
+package com.ecofinancebanking;
+
+@SpringBootApplication
+public class EcoFinanceBankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoFinanceBankingApplication.class, args);
+    }
+}
---
+++ src/main/java/com/ecofinancebanking/SpringApplication.java
@@ -0,0 +1,10 @@
+package com.ecofinancebanking;
+
+public class SpringApplication {
+
+    public static void run(Class<EcoFinanceBankingApplication> class1, String[] args) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+
+}
